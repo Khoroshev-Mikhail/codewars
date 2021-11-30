9693796f3f2b757ae842fb983a7bf39f045c6cd1
Mikha38 <oyks1379@yandex.ru>
2021-11-30 14:37:52 +0300

    131 completed

--- tasks.js
+++ tasks.js
@@ -746,3 +746,27 @@ function listFns(names){
 }
 
 console.log(listFns([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ]))
+
+
+
+
+
+console.log('----------------------------Task 131----------------------------')
+/*
+You've just finished writing the last chapter for your novel when a virus suddenly infects your document. 
+It has swapped the 'i's and 'e's in 'ei' words and capitalised random letters. Write a function which will:
+  a) remove the spelling errors in 'ei' words. (Example of 'ei' words: their, caffeine, deceive, weight)
+  b) only capitalise the first letter of each sentence. Make sure the rest of the sentence is in lower case.
+Example: He haD iEght ShOTs of CAffIEne. --> He had eight shots of caffeine.
+*/
+
+function proofread (str) { 
+  return str
+  .toLowerCase()
+  .replace( /ie/g, "ei")
+  .split('. ')
+  .map(string => string.charAt(0).toUpperCase() + string.slice(1))
+  .join('. ')
+} 
+
+console.log(proofread ("Their deceit was inconceivable. She seized the moment."))
