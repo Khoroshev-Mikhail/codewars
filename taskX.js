0b09615d6fd94426689e5d778672525aaac030d6
Mikha38 <oyks1379@yandex.ru>
2022-02-15 21:14:25 +0300

    home

--- taskX.js
+++ taskX.js
@@ -1,14 +1,13 @@
 console.log('----------------------------Task:309 "I Spy"----------------------------')
 function spyOn (func) {
   let count = 0
-  let cache = new Map()
-  function inner(x){
+  let cache = []
+  let cacheResults = []
+  function inner(...x){
     count++
-    let result = cache.get(x)
-    if(!result){
-      result = func(x)
-      cache.set(x, result)
-    }
+    let result = func(...x)
+    cache.push(...x)
+    cacheResults.push(result)
     return result
   }
 
@@ -16,20 +15,20 @@ function spyOn (func) {
     return count
   }
   inner.wasCalledWith = (val) => {
-    return cache.has(val)
+    return cache.includes(val)
   }
   inner.returned = (val) => {
-
+    return cacheResults.includes(val)
   }
-
   return inner
 }
 
-function func(x){
-  return x*x
+function func(...x){
+  return x+x
 }
 let spi = spyOn(func)
-console.log(spi(2))
-console.log(spi(2))
-console.log(spi(2))
-console.log(spi.callCount())
+//spi(2);
+spi('hello', 'hi', 'howdy');
+spi('goodbye', 'bye', 'see ya');
+console.log(spi.wasCalledWith('hi'))
+//console.log(spi.returned(4))
