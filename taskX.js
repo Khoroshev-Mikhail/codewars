1ff5f856406c5c78ff2c67d5c84907c92ab57c0f
Mikha38 <oyks1379@yandex.ru>
2022-02-15 16:53:17 +0300

    work

--- taskX.js
+++ taskX.js
@@ -4,7 +4,7 @@ function spyOn (func) {
   let cache = new Map()
   function inner(x){
     count++
-    let result = cache.get(n)
+    let result = cache.get(x)
     if(!result){
       result = func(x)
       cache.set(x, result)
@@ -19,9 +19,7 @@ function spyOn (func) {
     return cache.has(val)
   }
   inner.returned = (val) => {
-    if(cache.has(val)){
-      
-    }
+
   }
 
   return inner
@@ -30,6 +28,8 @@ function spyOn (func) {
 function func(x){
   return x*x
 }
-let fn = func(5)
-let spi = spyOn(fn)
-console.log(spi.ara())
+let spi = spyOn(func)
+console.log(spi(2))
+console.log(spi(2))
+console.log(spi(2))
+console.log(spi.callCount())
