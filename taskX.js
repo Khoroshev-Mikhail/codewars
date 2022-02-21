c19693ff3681f6b98fe4587da9c610e2255a738e
Mikha38 <oyks1379@yandex.ru>
2022-02-21 14:04:16 +0300

    work

--- taskX.js
+++ taskX.js
@@ -1,1 +1,21 @@
-console.log('----------------------------Task: ""----------------------------')
+console.log('----------------------------Task:316 "multiFilter"----------------------------')
+function isEven(el){
+    return el % 2 === 0;
+}
+
+function isGTten(el){
+    return el > 10;
+}
+let i = -1;
+var multiFilter = function(){
+    i++
+	return (x) =>{
+        if(i !== arguments.length - 1){
+            console.log('if')
+            return arguments[i](x) && multiFilter()
+        }
+        return arguments[arguments.length - 1]
+    }
+};
+//console.log(multiFilter(isEven, isGTten))
+console.log([1,2,3,4,10,11,12,20,21,22].filter(multiFilter(isEven, isGTten)))
