b92dd006733cc16715034172d5e64c30f873bc48
Mikha38 <oyks1379@yandex.ru>
2022-02-22 14:49:48 +0300

    work

--- taskX.js
+++ taskX.js
@@ -1,10 +1,13 @@
-console.log('----------------------------Task:317 "Combinator Flip"----------------------------')
-function flip(fn) {
-    return (...x) =>{
-        return fn(...x.reverse())
+console.log('----------------------------Task:XXX "Задача с https://learn.javascript.ru/"----------------------------')
+let company = { // тот же самый объект, сжатый для краткости
+    sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 600 }],
+    development: {
+      sites: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800 }],
+      internals: [{name: 'Jack', salary: 1300}]
     }
+};
+function sumSalaries(department){
+
+    
 }
-function print(a,b) {
-    return a + " -> " + b;
-}
-console.log(flip(print)(4, 5))
+console.log(sumSalaries(company))
