8cb7d3bae0f4516b8840a464fa7fbd27a6d473f0
Mikha38 <oyks1379@yandex.ru>
2022-02-22 12:25:26 +0300

    home

--- taskX.js
+++ taskX.js
@@ -1,17 +1,10 @@
-console.log('----------------------------Task:316 "multiFilter"----------------------------')
-function isEven(el){
-    return el % 2 === 0;
-}
-
-function isGTten(el){
-    return el > 10;
-}
-function islessThenFifteen(el){
-    return el < 15
-}
-var multiFilter = function(){
-	return (x) => {
-        return Object.values(arguments).every(fn => fn(x) === true)
+console.log('----------------------------Task:317 "Combinator Flip"----------------------------')
+function flip(fn) {
+    return (...x) =>{
+        return fn(...x.reverse())
     }
-};
-console.log([1,2,3,4,10,11,12,20,21,22].filter(multiFilter(isEven, isGTten, islessThenFifteen)))
+}
+function print(a,b) {
+    return a + " -> " + b;
+}
+console.log(flip(print)(4, 5))
--- tasks7.js
+++ tasks7.js
@@ -212,7 +212,18 @@ function islessThenFifteen(el){
 }
 var multiFilter = function(){
 	return (x) => {
-        return Object.values(arguments).every(fn => fn(x) === true)
+        return Object.values(arguments).every(fn => fn(x))
     }
 };
 console.log([1,2,3,4,10,11,12,20,21,22].filter(multiFilter(isEven, isGTten, islessThenFifteen)))
+
+console.log('----------------------------Task:317 "Combinator Flip"----------------------------')
+function flip(fn) {
+    return (...x) =>{
+        return fn(...x.reverse())
+    }
+}
+function print(a,b) {
+    return a + " -> " + b;
+}
+console.log(flip(print)(4, 5))
