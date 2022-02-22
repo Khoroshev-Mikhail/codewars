218fbb23d37ca903f6e766eaede665010d58e631
Mikha38 <oyks1379@yandex.ru>
2022-02-22 15:50:30 +0300

    work

--- taskX.js
+++ taskX.js
@@ -1,13 +1,8 @@
-console.log('----------------------------Task:XXX "Задача с https://learn.javascript.ru/"----------------------------')
-let company = { // тот же самый объект, сжатый для краткости
-    sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 600 }],
-    development: {
-      sites: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800 }],
-      internals: [{name: 'Jack', salary: 1300}]
-    }
+console.log('----------------------------Task:515 "Sum of a sequence"----------------------------')
+let i = 0;
+const sequenceSum = (begin, end, step) => {
+  if(i === end){
+    return 
+  }
 };
-function sumSalaries(department){
-
-    
-}
-console.log(sumSalaries(company))
+console.log(sequenceSum(2, 6, 2))
