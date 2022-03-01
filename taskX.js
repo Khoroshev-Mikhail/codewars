c3518850dfea56980258eb6246989325b0c8212d
Mikha38 <oyks1379@yandex.ru>
2022-03-01 22:57:26 +0300

    home

--- taskX.js
+++ taskX.js
@@ -1,8 +1,7 @@
-console.log('----------------------------Task:515 "Sum of a sequence"----------------------------')
-const sequenceSum = (begin, end, step) => {
-  if(begin > end){
-    return 0
-  }
-  return begin += sequenceSum(begin + step, end, step)
-};
-console.log(sequenceSum(1, 5, 1))
+console.log('----------------------------Task:522 "Sum squares of numbers in list that may contain more lists"----------------------------')
+function SumSquares(l){
+  return l.reduce((a, b) => {
+    return Array.isArray(b) ? a + SumSquares(b) : a + b**2
+  }, 0)
+}
+console.log(SumSquares([1, 5, 1, [[2]]]))
