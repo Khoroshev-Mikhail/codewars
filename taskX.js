5cd3b1b276db890e78bb63f6274fb7829415a04f
Mikha38 <oyks1379@yandex.ru>
2022-03-03 09:36:23 +0300

    ara

--- taskX.js
+++ taskX.js
@@ -1,8 +1,6 @@
 console.log('----------------------------Task:515 "Sum of a sequence"----------------------------')
 let i = 0;
 const sequenceSum = (begin, end, step) => {
-  if(i === end){
-    return 
-  }
+  
 };
 console.log(sequenceSum(2, 6, 2))
