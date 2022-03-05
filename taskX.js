e130b8c5181514e162fe1bbc69ae7eeda9f9bf9f
Mikha38 <oyks1379@yandex.ru>
2022-03-05 23:21:33 +0300

    peredelkino

--- taskX.js
+++ taskX.js
@@ -1,9 +1,20 @@
 console.log('----------------------------Task:506 "Flatten a Nested Map"----------------------------')
-function combos(){
-  return x
+let result = []
+function combos(x, i = 4){
+  let arr = [...x]
+  let el = Math.min(...arr)
+  let index = arr.indexOf(el)
+  arr[index]++
+  let res = arr.slice(0, index + 1);
+  let last = res[res.length-1]
+  while(res.reduce((a, b) => a + b, 0) <= 8){
+    res.push(last)
+  }
+  result.push(arr)
+  return res
 }
 
-console.log(combos([1, 1, 1, 1, 1]))
+console.log(combos([4, 1, 1, 1, 1, 1, 1]))
 
 
 
