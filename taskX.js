814af01da862f97254417efc5b7a3c3a358ca22e
Mikha38 <oyks1379@yandex.ru>
2022-03-22 08:01:05 +0300

    morning work

--- taskX.js
+++ taskX.js
@@ -1,59 +1,22 @@
-console.log('----------------------------Task:517 "Deep comparison of objects"----------------------------')
-//Перерефактори на return, а не arr.push()
-const obj1 = {
-  ara : 2,
-  bara : {
-    ggg : 3,
-    vvv : 6
+console.log('----------------------------Task:XXX "Binary Search"----------------------------')
+function binarySearch(arr, item, low = 0, hight = arr.length - 1){
+  let mid = low + Math.floor((hight - low) / 2)
+  if(low > hight){
+    return 'Такого значения в массиве нет!'
+  }
+  if(arr[mid] < item){
+    return binarySearch(arr, item, mid + 1, hight)
+  }
+  if(arr[mid] > item){
+    return binarySearch(arr, item, low, mid - 1)
+  }
+  if(arr[mid] === item){
+    
+    return mid
   }
 }
-const obj2 = {
-  ara : 2,
-  bara : {
-    ggg : 3,
-    vvv : 6
-  }
-}
-function deepCompare(o1, o2, arr = []) {
-  if(o1 === null || o2 === null){
-    arr.push(o1 === o2)
-  }
-  if(typeof o1 !== 'object' && typeof o2 !== 'object'){
-    arr.push(o1 === o2)
-  }
-  if(typeof o1 === 'object' && typeof o2 === 'object' && o1 !== null && o2 !== null){
-    //Если это массивы
-    if(Array.isArray(o1) && Array.isArray(o2)){
-      if(o1.length !== o2.length){
-        arr.push(false)
-      }
-      if(o1.length === 0 && o2.length === 0){
-        arr.push(true)
-      }
-    }
-    //Если это объекты
-    if(!Array.isArray(o1) && !Array.isArray(o2)){
-      if(Object.keys(o1).length !== Object.keys(o2).length){
-        arr.push(false)
-      }
-      if(Object.keys(o1).length === 0 && Object.keys(o2).length === 0){
-        arr.push(true)
-      }
-    }
-    //Если это массив и объект
-    if(!Array.isArray(o1) && Array.isArray(o2)){
-      arr.push(false)
-    }
-    for(let el in o1){
-      deepCompare(o1[el], o2[el], arr)
-    }
-  }
-  if(typeof o1 !== 'object' && typeof o2 !== 'object'){
-    arr.push(o1 === o2)
-  }
-  return !arr.includes(false)
-};
-console.log(deepCompare({}, {}))
+
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10,11], 12))
 
 
 
--- tasks9.js
+++ tasks9.js
@@ -1,3 +1,21 @@
+console.log('----------------------------Task:XXX "Binary Search"----------------------------')
+function binarySearch(arr, item, low = 0, hight = arr.length - 1){
+  let mid = low + Math.floor((hight - low) / 2)
+  if(low > hight){
+    return 'Такого значения в массиве нет!'
+  }
+  if(arr[mid] < item){
+    return binarySearch(arr, item, mid + 1, hight)
+  }
+  if(arr[mid] > item){
+    return binarySearch(arr, item, low, mid - 1)
+  }
+  if(arr[mid] === item){
+    return mid
+  }
+}
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10,11], 12))
+
 console.log('----------------------------Task:508 "Array Deep Count"----------------------------')
 function deepCount(arr){
   let sum = arr.length;
