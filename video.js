6edcd2f1f602a18761c210757a087f17fecead99
Mikha38 <oyks1379@yandex.ru>
2022-04-15 23:08:42 +0300

    home

--- video.js
+++ video.js
@@ -1,3 +1,6 @@
+/*
+
+
 const obj = {
     ara : 1
 }
@@ -6,20 +9,58 @@ let x = Object.getPrototypeOf(obj)
 x.prototype.sayHi = function(){
     return 1
 }
-console.log(x)
+
+*/
+//console.log(x.prototype)
+
+
 /*
-class Test{
+function fnsObj(name){
+    this.name = name
+}
+let z = new fnsObj('Batman')
+
+let parent1 = Object.getPrototypeOf(z)
+let parent2 = Object.getPrototypeOf(Object.getPrototypeOf(z))
+let parent3 = Object.getPrototypeOf(parent2)
+
+console.log(parent2)
+*/
+
+
+class objByClass{
     constructor(name){
         this.name = name
     }
 }
-let y = new Test('Gafa')
-Test.prototype.sayHi = function(){
+let y = new objByClass('Gafa')
+
+/*Test.prototype.sayHi = function(){
     return 'hello'
-}
-console.log(Object.getPrototypeOf(y))
-function newObj(name){
-    this.name = name
-}
-let z = new newObj('Batman')
+}*/
+/*
+let parent1 = Object.getPrototypeOf(y)
+let parent2 = Object.getPrototypeOf(parent1)
+let parent3 = Object.getPrototypeOf(parent2)
+console.log(parent3)
 */
+
+
+const obj = {
+    ara : 1
+}
+
+const ara = {
+    ggg : 2
+}
+Object.setPrototypeOf(ara, obj)
+
+
+let parent1 = Object.getPrototypeOf(ara)
+let parent2 = Object.getPrototypeOf(parent1)
+console.log(y.constructor)
+//let parent3 = Object.getPrototypeOf(parent2)
+/*x.prototype.sayHi = function(){
+    return 1
+}*/
+
