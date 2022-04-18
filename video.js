ce72dfbd009558cf139bd764cec2f5efc72e0a92
Mikha38 <oyks1379@yandex.ru>
2022-04-18 14:57:42 +0300

    work

--- video.js
+++ video.js
@@ -1,15 +1,6 @@
-let user = {
-    name: "John",
-    surname: "Smith"
-  };
-  Object.defineProperty(user, 'names', {
-    get() {
-      return `${this.name} ${this.surname}`;
-    },
-  
-    set(value) {
-      [this.name, this.surname] = value.split(" ");
-    }
-  });
-user.names = 'asd sdasdddd'
-console.log(user.names)
+let json = '{ "age": 30 }';
+try{
+    ara()
+    let user = JSON.parse(json)
+} catch(e){
+}
