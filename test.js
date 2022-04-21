b82d838f8c50a038fb0e076a87591afb37c1646b
Mikha38 <oyks1379@yandex.ru>
2022-04-21 13:07:05 +0300

    work

--- test.js
+++ test.js
@@ -1,28 +1,9 @@
-<<<<<<< HEAD
-=======
-function multiply (value, times) {
-    switch(typeof value){
-        case 'string': 
-            if(typeof times !== 'number' || !Number.isInteger(times)){
-                return new RangeError('Invalid count value')
-            }
-            return value.repeat(times)
-        case 'number':
-            return value * times
-        case 'object':
-            if(value === null){
-                return null
-            }
-            return new Array(times).fill(value)
-        case 'function':
-            return function(){
-                for(let i = 0; i < times; i++){
-                    value.call(this, ...arguments)
-                }
-            }
-        default:
-            return value
+function Animal(name){
+    this.name = name
+    this.x = function(){
+        return 'x'
     }
-  }
-  console.log(multiply('foo', 0.3))
->>>>>>> 1c9630a097b10bfa46ef45bab10323a0285684a5
+}
+let gafa = new Animal('Gafa')
+console.log(Animal.prototype)
+console.log(gafa)
