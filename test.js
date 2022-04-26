d8248a9b33b5c82f009f072b82554debbcdf72bb
Mikha38 <oyks1379@yandex.ru>
2022-04-26 15:14:21 +0300

    work

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
-console.log('----------------------------Task:411----------------------------')
-var addOne = function(e) { return e + 1 };
-var square = function(e) { return e * e }
-var bbb = function(e) { return e * 2 }
-Function.prototype.pipe = function(fn){
-    /*
-    const first = this
-    return function(el){
-        const main = first(el)
-        return fn(main)
-    }
-    */
-    return function(el){
-        return fn(this(el))
-    }.bind(this)
+//console.log('----------------------------Task----------------------------')
+//https://www.codewars.com/kata/54da5a58ea159efa38000836
+function findOdd(A) {
+    let int = new Set();
+    A.forEach(el => int.has(el) ? int.delete(el) : int.add(el));
+    return int.values().next().value;
 }
+//console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]))
 
-var result = addOne.pipe(square).pipe(bbb)
-console.log(result(5)); // 72
+
+//console.log('----------------------------Task----------------------------')
+//https://www.codewars.com/kata/5264d2b162488dc400000001
+function spinWords(string){
+    return string.split(' ').map(el => el.length >= 5 ? el.split('').reverse().join('') : el).join(' ')
+}
+//console.log(spinWords('Hey fellow warriors'))
+
+
+console.log('----------------------------Task----------------------------')
