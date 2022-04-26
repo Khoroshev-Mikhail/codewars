a7a43210f378e559439df640f3785aa3f42a4fbb
Mikha38 <oyks1379@yandex.ru>
2022-04-26 21:58:45 +0300

    home

--- test.js
+++ test.js
@@ -1,19 +1,56 @@
-//console.log('----------------------------Task----------------------------')
-//https://www.codewars.com/kata/54da5a58ea159efa38000836
-function findOdd(A) {
-    let int = new Set();
-    A.forEach(el => int.has(el) ? int.delete(el) : int.add(el));
-    return int.values().next().value;
+function Lazy() {
+    this.arrayOfFn = []
+    Lazy.prototype.add = function(fn, ...args){
+        this.arrayOfFn.push([fn, ...args])
+        return this
+    }
+    Lazy.prototype.invoke = function(...arr){
+        let result;
+        for(let i = 0; i < this.arrayOfFn.length; i++){
+            let [fn, ...args] = this.arrayOfFn[i]
+            result = fn(...arr)
+        }
+        console.log(result)
+        return result
+    }
 }
-//console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]))
+//console.log(new Lazy().add(max))           ==  undefined
+let ara = new Lazy()
+.add(filterNumbers)
+//.add(filterRange, 2, 7)
+//.add(max)
+.invoke(1, 8, 6, [], "7", -1, {v: 5}, 4)
 
 
-//console.log('----------------------------Task----------------------------')
-//https://www.codewars.com/kata/5264d2b162488dc400000001
-function spinWords(string){
-    return string.split(' ').map(el => el.length >= 5 ? el.split('').reverse().join('') : el).join(' ')
+
+
+
+
+
+
+
+
+
+
+
+
+function max() {
+    return Math.max.apply(null, arguments);
 }
-//console.log(spinWords('Hey fellow warriors'))
 
+function filterNumbers() {
+  return Array.prototype.filter.call(arguments, function(value) {
+    return isNumeric(value);
+  });
+}
 
-console.log('----------------------------Task----------------------------')
+function isNumeric(n) {
+  return !isNaN(n) && Number(n) === n;
+}
+
+function filterRange(min, max) {
+  var args = Array.prototype.slice.call(arguments, 2);
+  return Array.prototype.filter.call(args, function(value) {
+    return min <= value && value <= max;
+  });
+}
