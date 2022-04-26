1c7b99ba1646af8c37556d24c2392f6696c7d5c6
Mikha38 <oyks1379@yandex.ru>
2022-04-26 12:57:12 +0300

    work

--- Task13.js
+++ Task13.js
@@ -3,12 +3,17 @@ var addOne = function(e) { return e + 1 };
 var square = function(e) { return e * e }
 var bbb = function(e) { return e * 2 }
 Function.prototype.pipe = function(fn){
-    // const first = this
-    //Переделать через байнд
+    /*
+    const first = this
     return function(el){
-        const main = this(el)
+        const main = first(el)
         return fn(main)
     }
+    */
+    const first = this
+    return function(el){
+        return fn(this(el))
+    }.bind(this)
 }
 
 var result = addOne.pipe(square).pipe(bbb)
--- test.js
+++ test.js
@@ -1,98 +1,20 @@
-function sum(x, y) {
-    return x + y;
-  }
-  
-  function double(x) {
-    return sum(x, x);
-  }
-  
-  function minus (x, y) {
-    return x - y;
-  }
-  
-  function addOne(x) {
-    return sum(x, 1);
+console.log('----------------------------Task:411----------------------------')
+var addOne = function(e) { return e + 1 };
+var square = function(e) { return e * e }
+var bbb = function(e) { return e * 2 }
+Function.prototype.pipe = function(fn){
+    /*
+    const first = this
+    return function(el){
+        const main = first(el)
+        return fn(main)
+    }
+    */
+    const first = this
+    return function(el){
+        return fn(this(el))
+    }.bind(this)
 }
 
-function chain(fns) {
-    function Wrapper(x){
-        this.accum = x
-    }
-    for(fn in fns){
-        let func = fns[fn]
-        Wrapper.prototype[fn] = function(){
-            let args = Array.from(arguments)
-            if(args.accum){
-                args.unshift(this.accum)
-            }
-
-            let x = func.apply(null, args)
-            return new Wrapper(x)
-        }
-    }
-    Wrapper.prototype.execute = function(){
-        return this.accum
-    }
-    return new Wrapper()
-}
-var c = chain({sum: sum, minus: minus, double: double, addOne: addOne});
-
-console.log(c.sum(5, 6).execute())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var result = addOne.pipe(square).pipe(bbb)
+console.log(result(5)); // 72
