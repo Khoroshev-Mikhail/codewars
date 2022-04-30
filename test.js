78c00a5dd0aa4db1500d9867ee295e4eb3ed0c74
Mikha38 <oyks1379@yandex.ru>
2022-04-30 20:15:19 +0300

    home

--- test.js
+++ test.js
@@ -1,24 +1,8 @@
-function likes(names) {
-    switch(names.length){
-        case 0: 
-            return "no one likes this"
-        case 1: 
-            return `${names[0]} likes this`
-        case 2:
-            return `${names[0]} and ${names[1]} like this`
-        case 3: 
-            return `${names[0]}, ${names[1]} and ${names[2]} like this`
-        default:
-            return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+
+//https://www.codewars.com/kata/6161847f52747c0025d0349a/solutions/javascript
+const createNDimensionalArray = (n, size, k = n) => {
+    if(n === 1){
+        return new Array(size).fill(`level ${k}`) 
     }
+    return new Array(size).fill(createNDimensionalArray(n - 1, size, k))
   }
-
-  /*
-
-  []                                -->  "no one likes this"
-["Peter"]                         -->  "Peter likes this"
-["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
-["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
-["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
-
-*/
