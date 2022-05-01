b7b25b5ba6c68235e60e34c6e37fe75b92e2efae
Mikha38 <oyks1379@yandex.ru>
2022-05-01 22:04:31 +0300

    home

--- recursion.js
+++ recursion.js
@@ -1,19 +1,17 @@
-function pipeline(seed , ...args ) {
-    return args.reduceRight((acc, fn) => {
-        if(acc instanceof Array){
-            return fn.call(this,...acc)
-        }
-        return fn(acc)
-    }, seed)
-  };
-
-  function compose(...args) {
-    return (...x) => {
-        return args.reduceRight((acc, fn) => {
-            if(acc instanceof Array){
-                return fn(...acc)
-            }
-            return fn(acc)
-        }, x)
+function maxMatch(sentence, word = sentence[0], arr = []){
+    if(sentence.length === 0 || word === undefined){
+        return arr
     }
-  };
+    let hits = Array.from(VALID_WORDS).filter(el => el.slice(0, word.length) === word)
+    let nextHits = []
+    if(sentence.length > word.length){
+        let nextWord = sentence.slice(0, word.length + 1)
+        nextHits = Array.from(VALID_WORDS).filter(el => el.slice(0, nextWord.length) === nextWord)
+    }
+
+    if(hits.length >= 1 && nextHits.length === 0){    
+        arr.push(word)
+        return maxMatch(sentence.slice(word.length), sentence.slice(word.length)[0], arr)
+    }  
+    return maxMatch(sentence, sentence.slice(0, word.length + 1), arr)
+}
