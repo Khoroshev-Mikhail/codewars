8673474c680883322167108f093f9b557c95fad7
Mikha38 <oyks1379@yandex.ru>
2022-05-05 21:54:50 +0300

    home

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
-fetch('https://github.com/Mikha38/')
-  .then(response => response.json)
-  .then(response => console.log(response))
+function encode(string) {
+  return string.replace(/[aeiou]/g, (x) => {'_aeiou'.indexOf(x)})
+}
+function decode(string) {
+  return string.replace(/[1-5]/g, (x) => {'_aeiou'.charAt(x)})
+}
