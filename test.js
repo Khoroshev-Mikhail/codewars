946e860467086b64edae727433330e82a59fb79c
Mikha38 <oyks1379@yandex.ru>
2022-05-06 11:55:22 +0300

    home

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
-function encode(string) {
-  return string.replace(/[aeiou]/g, (x) => {'_aeiou'.indexOf(x)})
+function autocomplete(input, dictionary){
+  input = input.replace(/[\W\d\s]/gi, '')
+  const result = dictionary.filter(el => input.test(el))
+  return result.length <= 5 ? result : result.slice(0, 5)
 }
-function decode(string) {
-  return string.replace(/[1-5]/g, (x) => {'_aeiou'.charAt(x)})
-}
+
+console.log(autocomplete('ai', ['airplane','airport','apple','ball', 'ais']))
+
+/*
+Expected: '[\'airplane\', \'apple\', \'air\', \'avenue\', \'airport\']', 
+instead got: '[\'airplane\', \'apple\', \'air\', \'avenue\', \'airport\', \'adamantium\', \'awkwardness\', \'awesome\', \'amazing\']'
+*/
