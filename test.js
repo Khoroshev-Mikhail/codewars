534ab8b9c28c18973cc2a2fb8191f97a485e91ab
Mikha38 <oyks1379@yandex.ru>
2022-05-10 16:46:18 +0300

    home

--- test.js
+++ test.js
@@ -1,39 +1,43 @@
-const asyncFns = [
-  () => new Promise((resolve, reject) => setTimeout(()=>resolve(1), Math.random() * 1000)),
-  () => new Promise((resolve, reject) => setTimeout(()=>resolve(2), Math.random() * 1000)),
-  () => new Promise((resolve, reject) => setTimeout(()=>resolve(3), Math.random() * 1000)),
-  () => new Promise((resolve, reject) => setTimeout(()=>resolve(4), Math.random() * 1000)),
-  () => new Promise((resolve, reject) => setTimeout(()=>resolve(5), Math.random() * 1000)),
-  () => new Promise((resolve, reject) => setTimeout(()=>resolve(6), Math.random() * 1000)),
-  () => new Promise((resolve, reject) => setTimeout(()=>resolve(7), Math.random() * 1000)),
-]
-
-
-function allWithLimit(fns, limit){
+//Четверг 17:30
+function cube(x) {
   return new Promise((resolve, reject) => {
-    let arr = [...fns]
-    let count = fns.length;
-    let result = []
-    function helper(){
-      if(arr.length === 0){
-        return;
-      }
-      let i = fns.length - arr.length
-      arr.shift()().then(x => {
-        result[i] = x
-        count--
-        if(count > 0){
-          helper()
-        }
-        if(count === 0){
-          resolve(result)
-        }
-      })
-    }
-    for(let i = 0; i < limit; i++){
-      helper()
-    }
+      setTimeout(() => {
+          if (Math.random() < .9) {
+              reject("err " + x);
+          } else {
+              resolve(x ** 3);
+          }
+      }, 500);
   })
 }
 
-allWithLimit(asyncFns, 3).then(console.log)
+
+function withRetry(fn, attempts) {
+  return function(x){
+    return new Promise((resolve, reject) => {
+      function helper(){
+        fn(x)
+        .then(
+          el => {
+            resolve(el)
+          },
+          err => {
+            console.log(attempts)
+            attempts--
+            if(attempts === 0){
+              /*return*/ reject(err)
+            }
+            if(attempts > 0){ //Почему без этого if перменная attempts может уйти в минус? 
+                              //reject(err) не останавливает выполнение кода?
+              helper()
+            }
+        })
+      }
+      helper()
+    })
+  }
+}
+
+//cube(3).then(console.log)
+const cubeWithRetry = withRetry(cube, 4);
+cubeWithRetry(3).then(console.log, console.log); // 50% + 25% + 12.5% + 6.25% === 93.75% vs 6.25%
